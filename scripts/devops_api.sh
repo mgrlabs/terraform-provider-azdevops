4cc25887df392adaa0d9bac046c7916e78ed8057
Mark Revill <mark.revill@vibrato.com.au>
2019-02-16 16:36:49 +1100

    New method for marshalling JSON added

--- scripts/devops_api.sh
+++ scripts/devops_api.sh
@@ -6,7 +6,7 @@ curl -s -H "Authorization: Basic $PAT_ENCODED" \
   -X GET https://dev.azure.com/$AZDO_ORG/_apis/projects/d54cda26-9daa-42a7-a7ef-b4e7854bb8e9/properties?api-version=5.0-preview.1
 
 curl -s -H "Authorization: Basic $PAT_ENCODED" \
-  -X GET https://dev.azure.com/$AZDO_ORG/_apis/projects/2227dc88-9503-4c4e-8918-6da802e3ebe0/properties?api-version=5.0-preview.1 | jq
+  -X GET https://dev.azure.com/$AZDO_ORG/_apis/projects/f25b3d2d-8846-406a-abd7-7a302d5bc434/properties?api-version=5.0-preview.1 | jq
 
 curl -s -H "Authorization: Basic $PAT_ENCODED" \
   -X GET https://dev.azure.com/$AZDO_ORG/_apis/work/processes?api-version=5.0-preview.2
